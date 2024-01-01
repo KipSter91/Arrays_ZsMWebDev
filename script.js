a627a1c49007c31223877ecade044d5e724be9b4
Zsolt Marku <zsolt.marku910221@gmail.com>
2024-01-01 14:26:52 +0100

    finishing this lecture's first coding challenge

--- script.js
+++ script.js
@@ -84,3 +84,47 @@ const currenciesUnique = new Set(['GBP', 'GBP', 'USD', 'USD', 'EUR', 'USD', 'EUR
 currenciesUnique.forEach((value, _, set) => {
   console.log(`${value}: ${value}`);
 });
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+console.log('Coding challenge 1 (Working with Arrays)'.padStart(35, '-').padEnd(55, '-'));
+//data from Julia
+const dataJ1 = [3, 5, 2, 12, 7];
+const dataJ2 = [9, 16, 6, 8, 3];
+
+//data fron Kate
+const dataK1 = [4, 1, 15, 8, 3];
+const dataK2 = [10, 5, 6, 1, 4];
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  dogsJulia.splice(-2);
+  const corrJulData = dogsJulia.slice(-2);
+  const allAges = corrJulData.concat(dogsKate);
+  allAges.forEach((age, i, arr) => age >= 3
+    ?
+    console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`)
+    :
+    console.log(`Dog number ${i + 1} is still a puppy 🐶`))
+}
+console.log('Check data 1'.padStart(35, '-').padEnd(55, '-'));
+checkDogs(dataJ1, dataK1)
+console.log('Check data 2'.padStart(35, '-').padEnd(55, '-'));
+checkDogs(dataJ2, dataK2)
