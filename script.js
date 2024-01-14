5e217da2b65634afb2180a18f5bbcc4d341543f2
Zsolt Marku <zsolt.marku910221@gmail.com>
2024-01-14 10:34:56 +0100

    finishing challenge 2

--- script.js
+++ script.js
@@ -214,3 +214,60 @@ console.log(maxValue);
 //shorter style: arrow function and conditional (ternary) operator
 const maxValue2 = movements2.reduce((acc, cur) => acc > cur ? acc : cur, movements2[0])
 console.log(maxValue2);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+//////////////////////*/
+///////////////////////
+
+const dataJ = [5, 2, 4, 1, 15, 8, 3];
+const dataK = [16, 6, 10, 5, 6, 1, 4];
+
+//detailed way
+const calcAverageHumenAge = function (ages) {
+  const humanAges = ages.map(function (age) {
+    if (age <= 2) {
+      return age * 2
+    } else {
+      return 16 + age * 4
+    }
+  });
+  console.log(humanAges);
+  const filteredHumanAges = humanAges.filter(function (age) {
+    return age > 18
+  });
+  console.log(filteredHumanAges);
+  const avarageOfAges = Math.trunc(filteredHumanAges.reduce(function (acc, cur, i, arr) {
+    return acc + cur
+  }, 0) / filteredHumanAges.length)
+  console.log(avarageOfAges);
+};
+
+calcAverageHumenAge(dataJ);
+calcAverageHumenAge(dataK);
+
+//short, method chained way
+const calcAverageHumanAge2 = (ages) => {
+  return Math.trunc(ages
+    .map(age => age <= 2 ? age * 2 : 16 + age * 4)
+    .filter(age => age > 18)
+    .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0))
+}
+
+const avg1 = calcAverageHumanAge2(dataJ);
+const avg2 = calcAverageHumanAge2(dataK);
+
+console.log(avg1, avg2);
